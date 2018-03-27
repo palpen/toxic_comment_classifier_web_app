cc39ed3432a3dd41ef0168ae14880d0facebabeb
Palermo Penano <palermo.penano@mail.utoronto.ca>
2018-03-27 18:24:22 -0400

    reorder classes for better code readability

--- ml_app.py
+++ ml_app.py
@@ -24,18 +24,19 @@ basepath = os.path.abspath("./toxic_comment_classifier_web_app")  # important fo
 
 with open(basepath + '/models/tfidf_vectorizer_train.pkl', 'rb') as tfidf_file:
         tfidf_model = pickle.load(tfidf_file)
+
+with open(basepath + '/models/logistic_toxic.pkl', 'rb') as logistic_toxic_file:
+    logistic_toxic_model = pickle.load(logistic_toxic_file)
+with open(basepath + '/models/logistic_severe_toxic.pkl', 'rb') as logistic_severe_toxic_file:
+    logistic_severe_toxic_model = pickle.load(logistic_severe_toxic_file)
 with open(basepath + '/models/logistic_identity_hate.pkl', 'rb') as logistic_identity_hate_file:
     logistic_identity_hate_model = pickle.load(logistic_identity_hate_file)
 with open(basepath + '/models/logistic_insult.pkl', 'rb') as logistic_insult_file:
     logistic_insult_model = pickle.load(logistic_insult_file)
 with open(basepath + '/models/logistic_obscene.pkl', 'rb') as logistic_obscene_file:
     logistic_obscene_model = pickle.load(logistic_obscene_file)
-with open(basepath + '/models/logistic_severe_toxic.pkl', 'rb') as logistic_severe_toxic_file:
-    logistic_severe_toxic_model = pickle.load(logistic_severe_toxic_file)
 with open(basepath + '/models/logistic_threat.pkl', 'rb') as logistic_threat_file:
     logistic_threat_model = pickle.load(logistic_threat_file)
-with open(basepath + '/models/logistic_toxic.pkl', 'rb') as logistic_toxic_file:
-    logistic_toxic_model = pickle.load(logistic_toxic_file)
 
 
 @app.route('/')
@@ -55,24 +56,24 @@ def my_form_post():
 
     dict_preds = {}
 
+    dict_preds['pred_toxic'] = logistic_toxic_model.predict_proba(comment_term_doc)[:, 1][0]
+    dict_preds['pred_severe_toxic'] = logistic_severe_toxic_model.predict_proba(comment_term_doc)[:, 1][0]
     dict_preds['pred_identity_hate'] = logistic_identity_hate_model.predict_proba(comment_term_doc)[:, 1][0]
     dict_preds['pred_insult'] = logistic_insult_model.predict_proba(comment_term_doc)[:, 1][0]
     dict_preds['pred_obscene'] = logistic_obscene_model.predict_proba(comment_term_doc)[:, 1][0]
-    dict_preds['pred_severe_toxic'] = logistic_severe_toxic_model.predict_proba(comment_term_doc)[:, 1][0]
     dict_preds['pred_threat'] = logistic_threat_model.predict_proba(comment_term_doc)[:, 1][0]
-    dict_preds['pred_toxic'] = logistic_toxic_model.predict_proba(comment_term_doc)[:, 1][0]
 
     for k in dict_preds:
         perc = dict_preds[k] * 100
         dict_preds[k] = "{0:.2f}%".format(perc)
 
     return render_template('main.html', text=text,
+                           pred_toxic=dict_preds['pred_toxic'],
+                           pred_severe_toxic=dict_preds['pred_severe_toxic'],
                            pred_identity_hate=dict_preds['pred_identity_hate'],
                            pred_insult=dict_preds['pred_insult'],
                            pred_obscene=dict_preds['pred_obscene'],
-                           pred_severe_toxic=dict_preds['pred_severe_toxic'],
-                           pred_threat=dict_preds['pred_threat'],
-                           pred_toxic=dict_preds['pred_toxic'])
+                           pred_threat=dict_preds['pred_threat'])
 
 
 if __name__ == '__main__':
